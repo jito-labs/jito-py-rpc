1cebe8a407de9729d7ba5d6d698614ffd90cf610
mdr0id <mznode@gmail.com>
2024-07-23 13:31:52 -0700

    remove version

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name='jito_jsonrpc_sdk',
-    version='0.1',
     packages=find_packages(where='sdk'),
     package_dir={'': 'sdk'},
     install_requires=[
