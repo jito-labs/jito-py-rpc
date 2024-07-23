5b029bf77a355fb41f82cb817ab45f7aee7cb96c
mdr0id <mznode@gmail.com>
2024-07-23 10:03:59 -0700

    update calling sequence for proper hiearchy

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='jito_jsonrpc_sdk',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(where='sdk'),
+    package_dir={'': 'sdk'},
     install_requires=[
         "requests",
     ],
